723cc58c69c97af69716d535db6183d9194696f9
Deian-Marian Giurgev <dgiurgev@1-A-14.42heilbronn.de>
2024-02-18 20:02:00 +0100

    i

---
+++ include/push_swap.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dgiurgev <dgiurgev@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/13 16:16:52 by dgiurgev          #+#    #+#             */
+/*   Updated: 2024/02/17 20:23:49 by dgiurgev         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include <limits.h>
+# include <stdbool.h>
+
+typedef struct s_stack_node
+{
+	int					nbr;
+	int					index;
+	int					push_cost;
+	bool				above_median;
+	bool				cheapest;
+	struct s_stack_node	*target_node;
+	struct s_stack_node	*next;
+	struct s_stack_node	*prev;
+}						t_stack_node;
+
+#endif
